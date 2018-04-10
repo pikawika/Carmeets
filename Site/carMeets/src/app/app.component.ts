e807a9fb05d206415e904d4c5a0dc09e5944e9d4
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-04-10 14:26:46 +0200

    lokale meetings

--- Site/carMeets/src/app/app.component.ts
+++ Site/carMeets/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meeting } from './meeting/meeting.model';
 
 
 @Component({
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
-  title = 'app';
+  private _meetings = new Array<Meeting>();
+
+  constructor() {
+    const meeting1 = new Meeting("Dream Day 2018", new Date(2018, 8, 16), "Wetteren", "Meeting door French Car Driver en Dare2Dream", "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", ["french", "oldtimers", "supercars"], "https://frenchcardrivers.com/"); 
+    const meeting2 = new Meeting("2 Dream Day 2018", new Date(2020, 8, 16), "2Wetteren", "2Meeting door French Car Driver en Dare2Dream", "2Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.", ["2french", "2oldtimers", "2supercars"], "https://2frenchcardrivers.com/"); 
+	
+    this._meetings.push(meeting1);
+    this._meetings.push(meeting2);
+	
+  }
+
+  title = 'CarMeets.be';
 }
--- Site/carMeets/src/app/meeting/meeting.component.ts
+++ Site/carMeets/src/app/meeting/meeting.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import { Meeting } from "./meeting.model";
 
 @Component({
   selector: "app-meeting",
@@ -6,28 +7,10 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./meeting.component.css"]
 })
 export class MeetingComponent implements OnInit {
-  name: string;
-  date: Date;
-  site: string;
-  gemeente: string;
-  shortdescription: string;
-  fulldescription: string;
-  categories: string[];
-  likeAmount: number;
-  commentAmount: number;
-
+  @Input() public meeting:Meeting;
 
   constructor() {
-    this.name = "Dream Day 2018";
-    this.date = new Date(2018, 8, 16);
-    this.site = "https://frenchcardrivers.com/";
-    this.categories = ["french", "oldtimers", "supercars"];
-    this.shortdescription = "Meeting door French Car Driver en Dare2Dream";
-    this.fulldescription =
-      "Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent     tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non     fringilla.";
-    this.gemeente = "Wetteren";
-    this.likeAmount = 36;
-    this.commentAmount = 48;
+    
   }
 
   ngOnInit() {}
