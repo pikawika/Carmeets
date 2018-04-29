cb3a504f9328c7eca584d3ef19c0cfeb160c0937
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-04-29 23:59:05 +0200

    een gebruiker kan uit meerdere, vooraf gedefinieerde, categorien kiezen

--- Site/carMeets/frontend/src/app/user/account-page/add-meeting-page/add-meeting-page.component.ts
+++ Site/carMeets/frontend/src/app/user/account-page/add-meeting-page/add-meeting-page.component.ts
@@ -1,77 +1,135 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { AuthenticationService } from '../../authentication.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Meeting } from '../../../meeting/meeting.model';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, Validators, FormBuilder, FormArray, FormControl } from "@angular/forms";
+import { AuthenticationService } from "../../authentication.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Meeting } from "../../../meeting/meeting.model";
 
 @Component({
-  selector: 'app-add-meeting-page',
-  templateUrl: './add-meeting-page.component.html',
-  styleUrls: ['./add-meeting-page.component.css']
+  selector: "app-add-meeting-page",
+  templateUrl: "./add-meeting-page.component.html",
+  styleUrls: ["./add-meeting-page.component.css"]
 })
 export class AddMeetingPageComponent implements OnInit {
   public newMeetingFormGroup: FormGroup;
   public newMeetingErrorMsg: string;
+  public soortenMeetings: string[];
 
-  constructor(private newMeetingfb: FormBuilder, private authenticationService: AuthenticationService) { }
+  constructor(
+    private newMeetingfb: FormBuilder,
+    private authenticationService: AuthenticationService
+  ) {}
 
   ngOnInit() {
+    this.soortenMeetings = [
+      "American Muscle",
+      "Audio",
+      "British",
+      "Cabrio",
+      "Car limbo",
+      "Classics",
+      "Daily",
+      "Drifting",
+      "Exoctics",
+      "French",
+      "German",
+      "Grand tourers",
+      "Hatchbacks",
+      "Hypercars",
+      "Italian",
+      "JDM",
+      "Kit cars",
+      "Korean",
+      "Lowered",
+      "Movie cars",
+      "Off-road",
+      "Oldtimers",
+      "Racing",
+      "Show and shine",
+      "Sportcars",
+      "Stock",
+      "Supercars",
+      "Tuning",
+      "Underground",
+      "VAG"
+    ];
+
     this.newMeetingFormGroup = this.newMeetingfb.group({
-      name: [
-        '',
-        [Validators.required, Validators.maxLength(25)]
-      ],
+      name: ["", [Validators.required, Validators.maxLength(25)]],
       date: [
-        '',
-        [Validators.required, Validators.pattern("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$")]
+        "",
+        [
+          Validators.required,
+          Validators.pattern(
+            "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$"
+          )
+        ]
       ],
-      gemeente: [
-        '',
-        [Validators.required]
-      ],
-      postcode: [
-        '',
-        [Validators.required]
-      ],
-      straatnaam: [
-        '',
-        [Validators.required]
-      ],
-      straatnr: [
-        '',
-        [Validators.required]
-      ],
-      shortDescription: [
-        '',
-        [Validators.required, Validators.maxLength(80)]
-      ],
-      fullDescription: [
-        '',
-        [Validators.required, Validators.maxLength(1500)]
-      ],
-      categories: [
-        '',
-        [Validators.required]
-      ],
+      gemeente: ["", [Validators.required]],
+      postcode: ["", [Validators.required]],
+      straatnaam: ["", [Validators.required]],
+      straatnr: ["", [Validators.required]],
+      shortDescription: ["", [Validators.required, Validators.maxLength(80)]],
+      fullDescription: ["", [Validators.required, Validators.maxLength(1500)]],
+      categories: new FormArray([]),
       site: [
-        '',
-        [Validators.pattern("^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$")]
+        "",
+        [
+          Validators.pattern(
+            "^(http://www.|https://www.|http://|https://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$"
+          )
+        ]
       ]
     });
   }
 
+  onCheckChange(event) {
+    const formArray: FormArray = this.newMeetingFormGroup.get('categories') as FormArray;
+  
+    //gelsecteerde veld
+    if(event.target.checked){
+      // Toevoegen in array
+      formArray.push(new FormControl(event.target.value));
+    }
+    // verwijderde item
+    else{
+      // ittereren tot match
+      let i: number = 0;
+  
+      formArray.controls.forEach((ctrl: FormControl) => {
+        if(ctrl.value == event.target.value) {
+          formArray.removeAt(i);
+          return;
+        }
+  
+        i++;
+      });
+    }
+  }
+
   onSubmitAddMeeting() {
     let dateFrom = this.newMeetingFormGroup.value.date.split("/");
-    let date = new Date(dateFrom[2], dateFrom[1] - 1, dateFrom[0])
+    let date = new Date(dateFrom[2], dateFrom[1] - 1, dateFrom[0]);
 
     this.authenticationService
-      .addMeeting(new Meeting(this.newMeetingFormGroup.value.name, date, this.newMeetingFormGroup.value.gemeente, this.newMeetingFormGroup.value.postcode, this.newMeetingFormGroup.value.straatnaam,
-        this.newMeetingFormGroup.value.straatnr, this.newMeetingFormGroup.value.shortDescription, this.newMeetingFormGroup.value.fullDescription, this.newMeetingFormGroup.value.categories, this.newMeetingFormGroup.value.site))
+      .addMeeting(
+        new Meeting(
+          this.newMeetingFormGroup.value.name,
+          date,
+          this.newMeetingFormGroup.value.gemeente,
+          this.newMeetingFormGroup.value.postcode,
+          this.newMeetingFormGroup.value.straatnaam,
+          this.newMeetingFormGroup.value.straatnr,
+          this.newMeetingFormGroup.value.shortDescription,
+          this.newMeetingFormGroup.value.fullDescription,
+          this.newMeetingFormGroup.value.categories,
+          this.newMeetingFormGroup.value.site
+        )
+      )
       .subscribe(
         val => {
           if (val) {
             this.newMeetingErrorMsg = `Meeting toegevoegd!`;
-          }else{
+          } else {
             this.newMeetingErrorMsg = `Fout tijdens toevoegen meeting!`;
           }
         },
@@ -80,5 +138,4 @@ export class AddMeetingPageComponent implements OnInit {
         }
       );
   }
-
 }
