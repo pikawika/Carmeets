9ff782bd936fa76ee3ac16e1eb43ac488275101a
Lennert Bontinck <lennertbontinck@live.be>
2018-10-07 18:39:22 +0200

    And statement fix
    fixed a small issue with 2 and statements

--- Site/carMeets/backend/routes/meetings.js
+++ Site/carMeets/backend/routes/meetings.js
@@ -271,8 +271,7 @@ router.get("/getTotalLikedNext7D", authentication, function(req, res, next) {
   Meeting.find(
     {
       listUsersLiked: idGebruiker,
-      date: { $lte: new Date().getTime() + 7 * 1000 * 60 * 60 * 24 },
-      date: { $gte: new Date().getTime() }
+	  { $and: [ { date: { $lte: new Date().getTime() + 7 * 1000 * 60 * 60 * 24 } }, { date: { $gte: new Date().getTime() } } ] }
     },
 
     function(err, obj) {
@@ -295,8 +294,7 @@ router.get("/getTotalGoingNext7D", authentication, function(req, res, next) {
   Meeting.find(
     {
       listUsersGoing: idGebruiker,
-      date: { $lte: new Date().getTime() + 7 * 1000 * 60 * 60 * 24 },
-      date: { $gte: new Date().getTime() }
+      { $and: [ { date: { $lte: new Date().getTime() + 7 * 1000 * 60 * 60 * 24 } }, { date: { $gte: new Date().getTime() } } ] }
     },
 
     function(err, obj) {
